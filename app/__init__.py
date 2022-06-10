04275dba79268e5c30ad1e97b1d670412d1737d8
Gloria Labrador <gloriapatricia.labrador@ingka.ikea.com>
2022-06-10 11:42:30 +0200

    App v1

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@
 
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +14,19 @@ def create_app():
     api = Api()
     api.add_namespace(tweets)
     api.init_app(app)
+    
+    
 
+    from config import Config
+    app.config.from_object(Config)
+    db.init_app(app)
+    
+    from app.models import Tweet
+    from flask_migrate import Migrate
+    
+    migrate = Migrate(app, db)
+    
     app.config['ERROR_404_HELP'] = False
     return app
+
+    
